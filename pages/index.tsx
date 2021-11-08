ae09046cc100ff8a7bd35dd85bcc0a5e0781bd6c
Haider Alshamma <haideralsh@gmail.com>
2021-11-08 18:50:09 -0500

    Add interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,67 @@
 import type { NextPage } from "next";
 import { useState } from "react";
+import styled from "styled-components";
 
 const Home: NextPage = () => {
-  const [symbol, setSymbol] = useState("");
+  const [symbols, setSymbols] = useState("");
   const [result, setResult] = useState();
 
   const handleFind = async () => {
-    fetch("/api/chains/", { body: JSON.stringify({ symbol }), method: "POST" })
+    fetch("/api/chains/", {
+      body: JSON.stringify({ symbols: symbols.split(",") }),
+      method: "POST",
+    })
       .then((res) => res.json())
       .then((data) => JSON.parse(data.body))
       .then((body) => setResult(body.message));
   };
 
   return (
-    <>
-      <input value={symbol} onChange={(e) => setSymbol(e.target.value)} />
-      <button onClick={handleFind}>Find</button>
+    <Wrapper>
+      <SearchWrapper>
+        <SearchBox
+          placeholder="Search for one or more stock symbol"
+          value={symbols}
+          onChange={(e) => setSymbols(e.target.value)}
+        />
+        <Button onClick={handleFind}>Find</Button>
+      </SearchWrapper>
       <p>{JSON.stringify(result)}</p>
-    </>
+    </Wrapper>
   );
 };
+
+const SearchWrapper = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  color: #fff;
+  background-color: #000;
+  cursor: pointer;
+  border: 1px solid #000;
+  border-radius: 5px;
+  padding: 10px 20px;
+  margin: 5px;
+  font-size: 1.2em;
+`;
+
+const SearchBox = styled.input`
+  width: 450px;
+  height: 30px;
+  border: 1px solid #000;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 5px;
+  font-size: 1.2em;
+`;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default Home;
---
+++ test/go.mod
@@ -0,0 +1,3 @@
+module test/test
+
+go 1.17
