7d55bc561acd025b14877ec50c5ace638ecedb84
Haider Alshamma <haideralsh@gmail.com>
2021-12-12 17:38:19 -0500

    Restyle using tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from "next";
+import Image from "next";
 import { useState } from "react";
-import styled from "styled-components";
 
 const Home: NextPage = () => {
   const [symbols, setSymbols] = useState("");
+  const [percentage, setPercentage] = useState(12);
   const [result, setResult] = useState<any>({});
 
-  const handleFind = async () => {
+  const handleSubmit = async () => {
     fetch("/api/chains/", {
       body: JSON.stringify({ symbols: symbols.split(",") }),
       method: "POST",
@@ -16,76 +17,115 @@ const Home: NextPage = () => {
   };
 
   return (
-    <Wrapper>
-      <SearchWrapper>
-        <SearchBox
-          placeholder="Search for one or more stock symbol"
-          value={symbols}
-          onChange={(e) => setSymbols(e.target.value)}
-        />
-        <Button onClick={handleFind}>Find</Button>
-      </SearchWrapper>
-      {Object.keys(result).length > 0 && (
-        <table>
-          <thead>
-            <tr>
-              <td>Symbol</td>
-              <td>Expiration</td>
-              <td>Strike</td>
-              <td>Bid</td>
-              <td>Percentage</td>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(result).map((symbol) => {
-              return result[symbols]?.map((option: any) => (
-                <tr key={option.expiration}>
-                  <td>{symbol}</td>
-                  <td>{option?.expiration}</td>
-                  <td>{option?.strike}</td>
-                  <td>{option?.bid}</td>
-                  <td>{option?.percentage}</td>
-                </tr>
-              ));
-            })}
-          </tbody>
-        </table>
-      )}
-    </Wrapper>
+    <main className="flex h-full w-full justify-center">
+      <div className="flex flex-col mt-8">
+        <h1 className="text-2xl mb-6 text-gray-600">Optimal Chains</h1>
+        <form className="flex gap-4 items-end">
+          <div className="flex items-end gap-0.5">
+            <div className="flex flex-col w-72">
+              <label htmlFor="symbols" className="text-gray-500 text-sm pb-2">
+                Enter one or more symbols
+              </label>
+              <input
+                name="symbols"
+                className="bg-gray-100 focus:bg-gray-200 outline-none text-gray-700 uppercase p-1 px-3 rounded-l-full text-sm placeholder:text-gray-400 placeholder:text-sm placeholder:normal-case placeholder:font-sans"
+                maxLength={25}
+                placeholder="Example: AAPL, SPY, TSLA"
+                value={symbols}
+                onChange={(e) => setSymbols(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col w-18">
+              <label
+                htmlFor="percentage"
+                className="text-gray-500 text-sm pb-2"
+              >
+                Percentage
+              </label>
+              <input
+                name="percentage"
+                className="bg-gray-100 focus:bg-gray-200 text-gray-700 outline-none uppercase p-1 px-3 rounded-r-full text-sm placeholder:text-gray-400 placeholder:text-sm placeholder:normal-case placeholder:font-sans"
+                max={100}
+                min={0}
+                value={percentage}
+                type="number"
+                onChange={(e) => setPercentage(Number(e.target.value))}
+              />
+            </div>
+          </div>
+          <button
+            disabled={symbols.length === 0}
+            className="bg-teal-700 rounded-full text-white py-1 px-6 text-sm disabled:bg-gray-300 disabled:text-gray-400 "
+            onSubmit={handleSubmit}
+          >
+            Find
+          </button>
+        </form>
+        {Object.keys(result).length > 0 && (
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Symbol
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Expiration
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Strike
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Bid
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Percentage
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="bg-white divide-y divide-gray-200 text-sm">
+              {Object.keys(result).map((symbol) =>
+                result[symbol]?.map((option: any) => (
+                  <tr key={option.expiration}>
+                    <td className="px-6 py-4 whitespace-nowrap text-gray-500">
+                      {symbol}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-gray-500">
+                      {option.expiration}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-gray-500 text-right ">
+                      ${option.strike}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-right text-gray-500">
+                      ${option.bid}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-gray-500 text-sm font-medium">
+                      {Math.floor(option.percentage)}%
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </main>
   );
 };
-
-const SearchWrapper = styled.div`
-  display: flex;
-`;
-
-const Button = styled.button`
-  color: #fff;
-  background-color: #000;
-  cursor: pointer;
-  border: 1px solid #000;
-  border-radius: 5px;
-  padding: 10px 20px;
-  margin: 5px;
-  font-size: 1.2em;
-`;
-
-const SearchBox = styled.input`
-  width: 450px;
-  height: 30px;
-  border: 1px solid #000;
-  border-radius: 5px;
-  padding: 10px;
-  margin: 5px;
-  font-size: 1.2em;
-`;
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
 
 export default Home;
