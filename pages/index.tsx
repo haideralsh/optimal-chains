f9f153bb6961260ccb319fd8ed3f1ffa15f8230c
Haider Alshamma <haideralsh@gmail.com>
2021-12-12 19:14:05 -0500

    Change bare inputs to use a form

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,44 @@ import { useState } from "react";
 const Home: NextPage = () => {
   const [symbols, setSymbols] = useState("");
   const [percentage, setPercentage] = useState(12);
-  const [result, setResult] = useState<any>({});
 
-  const handleSubmit = async () => {
-    fetch("/api/chains/", {
-      body: JSON.stringify({ symbols: symbols.split(",") }),
-      method: "POST",
-    })
-      .then((res) => res.json())
-      .then((data) => setResult(data));
+  const [data, setData] = useState<any>({});
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const symbols = formData.get("symbols");
+
+    if (symbols && typeof symbols === "string") {
+      setLoading(true);
+
+      fetch("/api/chains/", {
+        body: JSON.stringify({ symbols: symbols?.split(",") }),
+        method: "POST",
+      })
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch(() => setError("An error has occurred."))
+        .finally(() => setLoading(false));
+    } else {
+      setError("Please enter valid symbols.");
+    }
   };
 
   return (
     <main className="flex h-full w-full justify-center">
-      <div className="flex flex-col mt-8">
+      <div className="flex flex-col mt-8 min-w-[500px]">
         <h1 className="text-2xl mb-6 text-gray-600">Optimal Chains</h1>
-        <form className="flex gap-4 items-end">
+        <form
+          className="flex gap-4 items-end"
+          name="optimal-chains"
+          onSubmit={handleSubmit}
+        >
           <div className="flex items-end gap-0.5">
-            <div className="flex flex-col w-72">
+            <fieldset className="flex flex-col w-72">
               <label htmlFor="symbols" className="text-gray-500 text-sm pb-2">
                 Enter one or more symbols
               </label>
@@ -34,8 +54,8 @@ const Home: NextPage = () => {
                 value={symbols}
                 onChange={(e) => setSymbols(e.target.value)}
               />
-            </div>
-            <div className="flex flex-col w-18">
+            </fieldset>
+            <fieldset className="flex flex-col w-18">
               <label
                 htmlFor="percentage"
                 className="text-gray-500 text-sm pb-2"
@@ -51,17 +71,16 @@ const Home: NextPage = () => {
                 type="number"
                 onChange={(e) => setPercentage(Number(e.target.value))}
               />
-            </div>
+            </fieldset>
           </div>
           <button
-            disabled={symbols.length === 0}
+            disabled={symbols.length === 0 || loading}
             className="bg-teal-700 rounded-full text-white py-1 px-6 text-sm disabled:bg-gray-300 disabled:text-gray-400 "
-            onSubmit={handleSubmit}
           >
-            Find
+            {loading ? "Loading..." : "Find"}
           </button>
         </form>
-        {Object.keys(result).length > 0 && (
+        {Object.keys(data).length > 0 && (
           <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-gray-50">
               <tr>
@@ -99,8 +118,8 @@ const Home: NextPage = () => {
             </thead>
 
             <tbody className="bg-white divide-y divide-gray-200 text-sm">
-              {Object.keys(result).map((symbol) =>
-                result[symbol]?.map((option: any) => (
+              {Object.keys(data).map((symbol) =>
+                data[symbol]?.map((option: any) => (
                   <tr key={option.expiration}>
                     <td className="px-6 py-4 whitespace-nowrap text-gray-500">
                       {symbol}
