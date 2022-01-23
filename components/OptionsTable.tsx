b70418823c20be7342554ce7e0d71a571fc21606
Haider Alshamma <haideralsh@gmail.com>
2022-01-22 23:29:16 -0500

    Extract to OptionsTable

---
+++ components/OptionsTable.tsx
@@ -0,0 +1,78 @@
+type Option = {
+  expiration: string;
+  strike: number;
+  bid: number;
+  percentage: number;
+};
+
+type OptionsTableProps = { symbolOptions: Record<string, Option[]> };
+
+const OptionsTableHead: React.FC = () => (
+  <thead className="bg-gray-50">
+    <tr>
+      <th
+        scope="col"
+        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+      >
+        Symbol
+      </th>
+      <th
+        scope="col"
+        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+      >
+        Expiration
+      </th>
+      <th
+        scope="col"
+        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+      >
+        Strike
+      </th>
+      <th
+        scope="col"
+        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+      >
+        Bid
+      </th>
+      <th
+        scope="col"
+        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+      >
+        Percentage
+      </th>
+    </tr>
+  </thead>
+);
+
+const OptionsTable: React.FC<OptionsTableProps> = ({ symbolOptions }) => {
+  return (
+    <table className="min-w-full divide-y divide-gray-200 mt-8">
+      <OptionsTableHead />
+      <tbody className="bg-white divide-y divide-gray-200 text-sm">
+        {Object.keys(symbolOptions).map((symbol) =>
+          symbolOptions[symbol].map((option: any) => (
+            <tr key={`${symbol}-${option.expiration}`}>
+              <td className="px-6 py-4 whitespace-nowrap text-gray-500 uppercase">
+                {symbol}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-gray-500">
+                {option.expiration}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-gray-500 text-right ">
+                ${option.strike}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-right text-gray-500">
+                ${option.bid}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-right text-gray-500 text-sm font-medium">
+                {Math.floor(option.percentage)}%
+              </td>
+            </tr>
+          ))
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+export default OptionsTable;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import OptionsTable from "../components/OptionsTable";
 
 const Home: NextPage = () => {
   const [symbols, setSymbols] = useState("");
@@ -87,71 +88,16 @@ const Home: NextPage = () => {
             {loading ? "Loading..." : "Find"}
           </button>
         </form>
-        {Object.keys(data).length > 0 && (
-          <table className="min-w-full divide-y divide-gray-200 mt-8">
-            <thead className="bg-gray-50">
-              <tr>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Symbol
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Expiration
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Strike
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Bid
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Percentage
-                </th>
-              </tr>
-            </thead>
-
-            <tbody className="bg-white divide-y divide-gray-200 text-sm">
-              {Object.keys(data).map((symbol) =>
-                data[symbol]?.map((option: any) => (
-                  <tr key={`${symbol}-${option.expiration}`}>
-                    <td className="px-6 py-4 whitespace-nowrap text-gray-500 uppercase">
-                      {symbol}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-gray-500">
-                      {option.expiration}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-gray-500 text-right ">
-                      ${option.strike}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-right text-gray-500">
-                      ${option.bid}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-right text-gray-500 text-sm font-medium">
-                      {Math.floor(option.percentage)}%
-                    </td>
-                  </tr>
-                ))
-              )}
-            </tbody>
-          </table>
+        {!isEmpty(data) ? (
+          <OptionsTable symbolOptions={data} />
+        ) : (
+          <p>There are no option chains for {symbols}</p>
         )}
       </div>
     </main>
   );
 };
+
+const isEmpty = (obj: Record<any, any>) => Object.keys(obj).length === 0;
 
 export default Home;
