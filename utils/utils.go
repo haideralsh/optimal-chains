9eddffe534925439ec99828e297ed8b348b73aa6
Haider Alshamma <haideralsh@gmail.com>
2022-07-03 15:13:38 -0400

    refactor utils to their own file

---
+++ utils/utils.go
@@ -0,0 +1,45 @@
+package utils
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+func SetCorsHeaders(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+}
+
+func BuildRequest(endpoint, token string) *http.Request {
+	u, _ := url.ParseRequestURI(endpoint)
+	url := u.String()
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	req.Header.Add("Accept", "application/json")
+
+	return req
+}
+
+func GetResponse(req *http.Request) []byte {
+	client := &http.Client{}
+
+	res, _ := client.Do(req)
+	data, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
+
+func FormatDate(date time.Time) string {
+	y, m, d := date.Date()
+	return fmt.Sprintf("%d-%d-%d", y, m, d)
+}
